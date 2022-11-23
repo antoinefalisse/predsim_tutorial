b2e31085223ecbf919c60eab7fe7adbc9fee6b62
Antoine Falisse <antoinefalisse@gmail.com>
2022-11-23 11:12:13 -0800

    clean up

--- settings.py
+++ settings.py
@@ -1,30 +1,22 @@
 def getSettings():
         
     settings = {        
-        # Hamner model
+        # Hamner model - 50 mesh intervals
         '0': {
             'model': 'Hamner_modified',
             'guessType': 'hotStart',
             'targetSpeed': 1.33,
             'N': 50,
             'modelMass': 62,
-            'dampingMtp': 1.9},     
+            'dampingMtp': 1.9},
         
+        # Hamner model - 25 mesh intervals (faster)
         '1': {
             'model': 'Hamner_modified',
             'guessType': 'hotStart',
             'targetSpeed': 1.33,
-            'N': 5,
+            'N': 25,
             'modelMass': 62,
-            'dampingMtp': 1.9},
-        
-        '2': {
-            'model': 'Hamner_modified',
-            'guessType': 'hotStart',
-            'targetSpeed': 1.33,
-            'N': 10,
-            'modelMass': 62,
-            'dampingMtp': 1.9},
-        }    
+            'dampingMtp': 1.9}}    
     
     return settings
